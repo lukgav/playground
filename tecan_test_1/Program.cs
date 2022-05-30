928e09aac10dd6c4d5c6302a44259b01eb9ac3af
luke <lukegavin.5@outlook.com>
2022-05-30 11:05:54 +0200

    + successfully reading in CSV file and transposing the csv into 2d array

--- tecan_test_1/Program.cs
+++ tecan_test_1/Program.cs
@@ -8,6 +8,10 @@
 // using System.IO;
 // using Microsoft.VisualBasic.FileIO.TextFieldParser
 
+//Question: What is an IDispoable Object???
+
+
+
 namespace TransposeData
 {
     class Program
@@ -16,20 +20,67 @@ namespace TransposeData
         {
             // string csvDir = "/home/luke/code/playground/tecan_test_1/TransposeData_Input.csv";
             string csvDir = "/home/luke/code/playground/tecan_test_1/TransposeData_Input.csv";
+            // EXCEPTION: Null
+            int rowLength = System.IO.File.ReadAllLines(csvDir).Length;
+            int columnLength = 0;
+            int currentColumnNumInFile = 0;
             using(var reader = new StreamReader(@csvDir))
             {
-                List<string> listA = new List<string>();
-                List<string> listB = new List<string>();
+
+                // List<string> listA = new List<string>();
+                // List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    // rowLength += 1;
+
+                    // Count number of rows to get row-size of array
+                    // Count number of columns to get column-size of array
+                    var line = reader.ReadLine();
+                    // EXCEPTION: Null
+                    var values = line.Split(';');
+                    columnLength = values.Length;
+                }
+            }
+            string [,] arr = new string[columnLength, rowLength];
+            using(var reader = new StreamReader(@csvDir))
+            {
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
+                    // EXCEPTION: Null
                     var values = line.Split(';');
-
-                    listA.Add(values[0]);
-                    listB.Add(values[1]);
-                    Console.WriteLine(values[0]);
-
+                    for(int i=0;i < columnLength-1; i++)
+                    {
+                        // foreach(int j=0;j<rowLength-1; j++)
+                        // {
+                        arr[i,currentColumnNumInFile] = values[i] + ";";
+                        Console.Write("{0};", values[i]);
+                    }
+                    Console.WriteLine("");
+                    currentColumnNumInFile += 1;
                 }
+
+
+
+                Console.WriteLine("Current Array: ");
+                for(int i=0;i < columnLength-1; i++)
+                {
+                    for(int j=0;j < rowLength; j++)
+                    {
+                        // foreach(int j=0;j<rowLength-1; j++)
+                        // {
+                        // arr[i,currentColumnNumInFile] = values[i];
+                        Console.Write("{0}", arr[i,j]);
+                    }
+                    Console.WriteLine();
+                }
+                // Console.WriteLine(rowLength);
+                // Console.WriteLine(columnLength);
+                
+                
+
+
+
             }
             //read in data into a 2d Array
             // transpose data thw 2d array
