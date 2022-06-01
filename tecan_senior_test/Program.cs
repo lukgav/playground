33b90c38fcb5db99ff7eed0eb6e8db8f5235b96f
luke <lukegavin.5@outlook.com>
2022-06-01 18:03:21 +0200

    - removed files

--- tecan_senior_test/Program.cs
+++ tecan_senior_test/Program.cs
@@ -1,9 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+//// Done: 
+// Create Sorter
+// Create generator
+//// TODO: 
+// Convert numbers to colors
+// Create UI
+//Split up features and characteristics into classes: generator Class, Sorter Class, UI Class, (Colormap class?)
 namespace ColorSorter
 {
-    class Program
-    {
 
+    public class Model{
+        // Requires Generator
+        // Constructor
+        private Random rand;
+        public int rowLength;
+        public int columnLength;
+        int[,] ColourArray;
+
+        public Model(int pRowLength, int pColumnLength)
+        {
+            rand = new Random(1000);
+            rowLength = pRowLength;
+            columnLength = pColumnLength;
+            ColourArray = new int[rowLength, columnLength];
+        }
+
+        public void generate2DArray(){
+            for (int i=0; i<this.rowLength; i++){
+                for (int j=0; j<this.columnLength; j++){
+                    this.ColourArray[i,j] = rand.Next(255);
+                }
+            }
+
+            //Print out column
+            for (int i=0; i<this.rowLength; i++){
+                for (int j=0; j<this.columnLength; j++){
+                    Console.Write("{0};",this.ColourArray[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------------");
+
+            return this.ColourArray;
+        }
+
+    }
+
+    // class View{
+
+
+    //     static void Color(){
+    //         public static System.Drawing.Color FromArgb (int argb);
+    //     }
+    // }
+
+    class Controller
+    {
         static int[,] InsertionSort2d(int[,] arr, int columnNumber)
         {
             int rowLength = arr.GetLength(0); 
@@ -29,37 +83,41 @@ namespace ColorSorter
 
         static void Main(string[] args)
         {
-
-            Random rand = new Random(1000);
+            //Put into Model
             int rowLength = 10;
             int columnLength = 10;
             int[,] ColourArray = new int[rowLength, columnLength];
+            Model model = new Model(rowLength, columnLength);
 
-            for (int i=0; i<rowLength; i++){
-                for (int j=0; j<columnLength; j++){
-                    ColourArray[i,j] = rand.Next(255);
-                }
-            }
+            ColourArray = model.generate2DArray();
 
-            //Print out column
-            for (int i=0; i<rowLength; i++){
-                for (int j=0; j<columnLength; j++){
-                    Console.Write("{0};",ColourArray[i,j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("---------------------------");
+            // int[,] ColourArray = new int[rowLength, columnLength];
 
-            int[,] sortedColourArray = new int[rowLength, columnLength];
-            for (int j=0; j<columnLength; j++){
-                sortedColourArray = InsertionSort2d(ColourArray, j);
-            }
-            for (int i=0; i<rowLength; i++){
-                for (int j=0; j<columnLength; j++){
-                    Console.Write("{0};",sortedColourArray[i,j]);
-                }
-                Console.WriteLine();
-            }
+            // for (int i=0; i<rowLength; i++){
+            //     for (int j=0; j<columnLength; j++){
+            //         ColourArray[i,j] = rand.Next(255);
+            //     }
+            // }
+
+            // //Print out column
+            // for (int i=0; i<rowLength; i++){
+            //     for (int j=0; j<columnLength; j++){
+            //         Console.Write("{0};",ColourArray[i,j]);
+            //     }
+            //     Console.WriteLine();
+            // }
+            // Console.WriteLine("---------------------------");
+
+            // int[,] sortedColourArray = new int[rowLength, columnLength];
+            // for (int j=0; j<columnLength; j++){
+            //     sortedColourArray = InsertionSort2d(ColourArray, j);
+            // }
+            // for (int i=0; i<rowLength; i++){
+            //     for (int j=0; j<columnLength; j++){
+            //         Console.Write("{0};",sortedColourArray[i,j]);
+            //     }
+            //     Console.WriteLine();
+            // }
 
             //Create random color generator
                 //Should be in 2d array
